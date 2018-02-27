69f1a9cb916b2995843c3ea9b988da46c9dd65c7
ggilmore <geoffrey@sourcegraph.com>
2018-02-27 11:48:34 -0800

    add support for arbitrary filesystems in parser

--- requirements/parser.py
+++ requirements/parser.py
@@ -4,7 +4,7 @@ import warnings
 from .requirement import Requirement
 
 
-def parse(reqstr):
+def parse(reqstr, current_path = "", file_system = None):
     """
     Parse a requirements file into a list of Requirements
 
@@ -18,10 +18,12 @@ def parse(reqstr):
         # Python 2.x compatibility
         if not isinstance(reqstr, basestring):
             reqstr = reqstr.read()
+            current_path = filename
     except NameError:
         # Python 3.x only
         if not isinstance(reqstr, str):
             reqstr = reqstr.read()
+            current_path = filename
 
     for line in reqstr.splitlines():
         line = line.strip()
@@ -32,11 +34,17 @@ def parse(reqstr):
             continue
         elif line.startswith('-r') or line.startswith('--requirement'):
             _, new_filename = line.split()
-            new_file_path = os.path.join(os.path.dirname(filename or '.'),
-                                         new_filename)
-            with open(new_file_path) as f:
-                for requirement in parse(f):
-                    yield requirement
+            new_file_path = os.path.join(os.path.dirname(current_path), 
+                                        new_filename)
+
+            if not file_system:
+                file_system = LocalFileSystem()
+
+            new_req_file = file_system.open(new_file_path)
+            
+            for requirement in parse(new_req_file, new_file_path, file_system):
+                yield requirement
+
         elif line.startswith('-f') or line.startswith('--find-links') or \
                 line.startswith('-i') or line.startswith('--index-url') or \
                 line.startswith('--extra-index-url') or \
@@ -48,3 +56,8 @@ def parse(reqstr):
             continue
         else:
             yield Requirement.parse(line)
+
+class LocalFileSystem:
+    def open(self, path):
+        with open(path, "r") as f:
+            return f.read()
